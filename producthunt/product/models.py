e2b833e2a1afc94c9be5139452faadb5332db0d2
kaustubhshukla30 <kaustubhshukla30@gmail.com>
2020-05-11 00:39:46 +0530

    migrations added

--- producthunt/product/models.py
+++ producthunt/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.TimeField(auto_now=False, auto_now_add=False)
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/', height_field=None, width_field=None, max_length=None)
+    icon  = models.ImageField(upload_to= 'images/', height_field=None, width_field=None, max_length=None)
+    url = models.CharField(max_length=50)
+    votes = models.IntegerField(default = 1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+    
